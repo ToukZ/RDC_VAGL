3acec0756ca655aae4d814c071b2975906f80999
Vinícius Araújo <vag2@ecomp.poli.br>
2022-04-06 15:28:31 -0300

    + comentários e uns consertos aí

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import socket
 
 h = open('index.html', 'r') #h é a variável que recebe o nosso site
-homepage = h.read() #homepage é a variável que possui em bytes o site
+homepage = h.read() #homepage é a variável que possui em formato string o site
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #abertura do socket para conexão
 s.bind(('', 42069)) #abrir o socket localmente com porta 42069
 s.listen(1) #espera receber 1 conexão
 
 while True: #enquanto tiver conexão, vai continuar rodando
-    conn, addr = s.accept() #variável que salva os dados da conexao
+    conn = s.accept() #variável que salva os dados da conexao
     data = conn.recv(2000) #variável que aceita até 2000 bytes dos dados recebidos
     info = data.split(b' ') #variável que separa os dados recebidos por espaços em um vetor
 
@@ -20,16 +20,16 @@ while True: #enquanto tiver conexão, vai continuar rodando
             conn.sendall(resp)
         else: #envie o arquivo requerido pela request
             print('file was sent: ' + str(info[1]))
-            ext = str(info[1].rpartition(b'.')[-1])
-            f = open(info[1][1:], 'rb')
-            fileRequested = f.read()
+            ext = str(info[1].rpartition(b'.')[-1]) #ext se refere à extensão do arquivo
+            f = open(info[1][1:], 'rb') #variável que abre o arquivo
+            fileRequested = f.read() #variável que possue o arquivo em formato de bytes
             resp = str.encode('HTTP/1.0 200 OK\r\n' +
                          'Content-Type: file' + ext + '\r\n' +
                          'Content-Length: ' + str(len(fileRequested)) + '\r\n\r\n')
-            conn.sendall(resp + fileRequested)
-        conn.close()
-    else: #se não há pedido por dados, é uma conexão mal formulada
-        print('did not get a request')
+            conn.sendall(resp + fileRequested) #envia em bytes o cabeçalho + o arquivo
+        conn.close() #fechamento de conexão, os arquivos já foram processados.
+    else: #se não há pedido por dados (ausência de GET), é uma conexão mal formulada
+        print('did not get the request')
         resp = str.encode('HTTP/1.0 469 BAD REQUEST\r\n')
         conn.sendall(resp)
 
